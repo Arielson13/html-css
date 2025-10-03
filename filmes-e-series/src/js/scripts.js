23610d40cd8276a88a990d4848f6ec80c0e168d3
Arielson13 <arielsonsousa37@gmail.com>
2025-10-03 17:22:23 -0300

    scripts adicionado

--- filmes-e-series/src/js/scripts.js
+++ filmes-e-series/src/js/scripts.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = "d99307ff";
-  const sectionMovies = document.querySelector('.sectionMovies');
+  const sectionMovies = document.querySelector(".sectionMovies");
 
   getMovie();
 
   async function getMovie() {
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&`
+      `http://www.omdbapi.com/?apikey=${apiKey}&s=2020`
     );
     const movie = await response.json();
-    console.log(movie);
-    showMovies(movie);
-  };
-
-  function showMovies(movie){
-    sectionMovies.innerHTML = `
-        <div>
-            <h3>${movie.Title}</h3>
-        </div>
-    `;
+    console.log(movie.Search);
+    showMovies(movie.Search);
   }
+
+  function showMovies(movie) {
+  sectionMovies.innerHTML = ""; // limpa antes de renderizar
+
+  movie.forEach((movies) => {
+    if (movies.Poster && movies.Poster !== "N/A") {
+      sectionMovies.innerHTML += `
+        <div>
+          <img src="${movies.Poster}" onerror="this.style.display='none'" />
+          <h3>${movies.Title}</h3>
+          <p>${movies.Year}</p>
+        </div>
+      `;
+    }
+  });
+}
 });
