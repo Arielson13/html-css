42459125a380154c6600078962017fe8616f0b18
Arielson13 <arielsonsousa37@gmail.com>
2025-10-04 13:09:24 -0300

    Consuming API and printing data. Adding styling to the information printed in the interface.

--- filmes-e-series/src/js/scripts.js
+++ filmes-e-series/src/js/scripts.js
@@ -1,31 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const apiKey = "d99307ff";
-  const sectionMovies = document.querySelector(".sectionMovies");
+const apikey = "9d1c1f083fcc4b5b345c477285200577";
+const baseUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=pt-BR&page=1`;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-  getMovie();
+const sectionMovies = document.querySelector(".sectionMovies");
 
-  async function getMovie() {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&s=2020`
-    );
-    const movie = await response.json();
-    console.log(movie.Search);
-    showMovies(movie.Search);
+async function fetchData() {
+  try {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    console.log(data.results);
+    printMovies(data.results);
+  } catch (error) {
+    console.error("Erro ao buscar filmes", error);
   }
+}
 
-  function showMovies(movie) {
-  sectionMovies.innerHTML = ""; // limpa antes de renderizar
+function printMovies(movies) {
+  sectionMovies.innerHTML = "";
 
-  movie.forEach((movies) => {
-    if (movies.Poster && movies.Poster !== "N/A") {
-      sectionMovies.innerHTML += `
-        <div>
-          <img src="${movies.Poster}" onerror="this.style.display='none'" />
-          <h3>${movies.Title}</h3>
-          <p>${movies.Year}</p>
-        </div>
-      `;
-    }
+  movies.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie");
+
+    movieCard.innerHTML = `
+      <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+      <p>⭐ ${movie.vote_average}</p>
+    `;
+
+    sectionMovies.appendChild(movieCard);
   });
 }
-});
+
+fetchData();
