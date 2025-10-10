7a5ea227bdf8721c5b2edf6d8beaab02d2544d02
Arielson13 <arielsonsousa37@gmail.com>
2025-10-10 15:11:10 -0300

    add new project

---
+++ portfolio/src/js/scripts.js
@@ -0,0 +1,9 @@
+// Efeito simples de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
